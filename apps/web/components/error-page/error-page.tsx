fb6bc5e8cda7de679531429a2f8e20d54d254ce2
hamxxn <150767569+hamxxn@users.noreply.github.com>
2025-07-18 19:17:13 +0900

    feat: error 컴포넌트 생성

---
+++ apps/web/components/error-page/error-page.tsx
@@ -0,0 +1,23 @@
+import { Button } from '@/components';
+import { SvgError } from '@/icons';
+
+export default function ErrorPage() {
+  return (
+    <div className="flex w-full flex-col items-center gap-[2.4rem]">
+      <div className="flex h-[10rem] w-[10rem] shrink-0 items-center justify-center rounded-[10rem] bg-pink-100 p-[3rem]">
+        <SvgError className="text-pink-300" />
+      </div>
+      <p className="jp-body1 font-bold text-gray-800">
+        問題が発生しました。 しばらくしてからもう一度お試しください。
+      </p>
+      <Button
+        color="primary"
+        size="md"
+        variant="filled"
+        className="jp-title2 rounded-[0.8rem] font-bold"
+      >
+        ホームに行く
+      </Button>
+    </div>
+  );
+}
